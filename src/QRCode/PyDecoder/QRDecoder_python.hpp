300ab953cb0c9e0f586f3bc7258d84ffe12561f5
skye347 <s347419313@gmail.com>
2017-03-15 20:01:57 +0800

    PyDecoder:add error handle

--- src/QRCode/PyDecoder/QRDecoder_python.hpp
+++ src/QRCode/PyDecoder/QRDecoder_python.hpp
@@ -21,19 +21,31 @@ class QRDecoder_python : public QRDecoderBase
     }
     std::string Decoder(const std::string &ImgPath)
     {
-        Py_Initialize();
-        py::object global(py::import("__main__").attr("__dict__"));
-        py::object pyFile=py::exec_file("static/qrdecoder.py", global, global);
-        pyDecoder=global["decoder"];
-        std::cout<<"extract"<<std::endl;
-        //execute method decoder
-        std::string val = py::extract<std::string>(pyDecoder(ImgPath));
-        return val;
+        try{
+            std::cout<<"extract"<<std::endl;
+            //execute method decoder
+            std::string val = py::extract<std::string>(pyDecoder(ImgPath));
+            return val;
+        }
+        catch(boost::python::error_already_set&){
+            PyErr_Print();
+            return 'error';
+        }
     }
   private:
     bool Init()
     {
-        return true;
+        try{
+            Py_Initialize();
+            py::object global(py::import("__main__").attr("__dict__"));
+            py::object pyFile=py::exec_file("static/qrdecoder.py", global, global);
+            pyDecoder=global["decoder"];
+            return true;
+        }
+        catch(boost::python::error_already_set&){
+            PyErr_Print();
+            return false;
+        }
     }
     bool isInit;
     py::object pyDecoder;
